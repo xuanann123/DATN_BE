2a4ff06f50abf61d4aae4574b1b834c692358055
tus204 <tuddaph43370@fpt.edu.vn>
2024-09-07 15:08:45 +0700

    refactor users table: email_verified_at, verification_token, rememberToken

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -18,7 +17,9 @@ return new class extends Migration
             $table->string('password');
             $table->string('avatar')->nullable();
             $table->boolean('is_active')->default(false);
-            $table->boolean('user_verify')->default(false);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('verification_token')->nullable();
+            $table->rememberToken();
             $table->string('user_type');
             $table->timestamps();
         });
