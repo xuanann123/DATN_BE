3fcb3a98499563943e98dac94566273113c57c76
xuananfpt <annxph37114@fpt.edu.vn>
2024-09-20 10:28:01 +0700

    feature/optimize_controller_categories

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -12,18 +12,54 @@ use Illuminate\Support\Facades\Storage;
 class CategoryController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
+        $listAct = [
+            "active" => "Hoạt động tất cả",
+            "inactive" => "Tắt hoạt động tất cả",
+            "trash" => "Xoá toàn bộ",
+        ];
         $title = "Danh sách danh mục";
-        $categories = Category::with('parent:id,name')->orderbyDesc('id')->paginate(10);
-        return view('admin.categories.index', compact('categories', 'title'));
+        $keyword = "";
+        if ($request->input('keyword')) {
+            $keyword = $request->input('keyword');
+        }
+        $status = $request->query('status', 'all');
+        // Lọc categories và đồng thời thay đổi giá trị listAct
+        $categories = Category::when($status != 'all', function ($query) use ($status, &$listAct) {
+            match ($status) {
+                'active' => $query->where('is_active', 1) && $listAct = [
+                    "inactive" => "Tắt hoạt động tất cả",
+                    "trash" => "Xoá toàn bộ",
+                ],
+                'inactive' => $query->where('is_active', 0) && $listAct = [
+                    "active" => "Hoạt động tất cả",
+                    "trash" => "Xoá toàn bộ",
+                ],
+                'trash' => $query->onlyTrashed() && $listAct = [
+                    "restore" => "Khôi phục toàn bộ",
+                    "forceDelete" => "Xoá cứng toàn bộ",
+                ],
+                default => null
+            };
+        })->where('name', 'like', "%$keyword%")->latest("id")->paginate(10);
+
+        $count = [
+            'all' => Category::count(),
+            'active' => Category::where('is_active', 1)->count(),
+            'inactive' => Category::where('is_active', 0)->count(),
+            'trash' => Category::onlyTrashed()->count(),
+        ];
+        //Danh sách trước khi chưa tối ưu
+        // $categories = Category::with('parent:id,name')->orderbyDesc('id')->paginate(10);
+        return view('admin.categories.index', compact('categories', 'title', 'listAct', 'count'));
     }
 
     private function getCategoryOptions($categories, $level = 0)
     {
         $options = [];
         foreach ($categories as $category) {
-            $prefix = str_repeat('&nbsp;&nbsp;', $level * 4) . ($level > 0 ? ' ' : '');
+            $prefix = str_repeat('-', $level * 2) . ($level > 0 ? ' ' : '');
             $options[$category->id] = $prefix . $category->name;
             if ($category->children->isNotEmpty()) {
                 $options += $this->getCategoryOptions($category->children, $level + 1);
@@ -31,7 +67,6 @@ class CategoryController extends Controller
         }
         return $options;
     }
-
 
     public function create()
     {
@@ -44,12 +79,9 @@ class CategoryController extends Controller
 
     public function store(CreateCategoryRequest $request)
     {
-
+        //Lấy toàn bộ dữ liệu trừ dữ liệu ảnh
         $data = $request->except('image');
-
-        if (!$request->is_active) {
-            $data['is_active'] = 0;
-        }
+        $data['is_active'] = $request->is_active ? $request->is_active : 0;
 
         if ($request->image && $request->hasFile('image')) {
             $image = $request->file('image');
@@ -68,31 +100,23 @@ class CategoryController extends Controller
     }
 
 
-    public function edit(string $id)
+    public function edit(Category $category)
     {
         $title = "Chỉnh sửa danh mục";
         $categories = Category::whereNull('parent_id')
-            ->where('id', '!=', $id)
+            ->where('id', '!=', $category->id)
             ->with('children')->get();
         $options = $this->getCategoryOptions($categories);
-        $category = Category::find($id);
         return view('admin.categories.edit', compact('options', 'category', 'title'));
     }
 
 
-    public function update(UpdateCategoryRequest $request, string $id)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-
         $data = $request->except('image');
 
         if (!$request->is_active) {
             $data['is_active'] = 0;
-        }
-
-        $category = Category::find($id);
-
-        if (!$category) {
-            return redirect()->route('admin.categories.index')->with(['error' => 'Danh mục không tồn tại!']);
         }
 
         if ($request->image && $request->hasFile('image')) {
@@ -120,23 +144,85 @@ class CategoryController extends Controller
     }
 
 
-    public function destroy(string $id)
+    public function destroy(Category $category)
     {
-        $category = Category::find($id);
 
-        if (!$category) {
-            return redirect()->route('admin.categories.index')->with(['error' => 'Danh mục không tồn tại!']);
+        // if ($category->image) {
+        //     $fileExists = Storage::disk('public')->exists($category->image);
+        //     if ($fileExists) {
+        //         Storage::disk('public')->delete($category->image);
+        //     }
+        // }
+        $category->delete();
+        return back()->with(['success' => 'Xóa thành công!']);
+    }
+
+
+    public function action(Request $request)
+    {
+        //Kiểm tra danh sách bản ghi tồn tại không
+        $listCheck = $request->listCheck;
+        if (!$listCheck) {
+            return redirect()->route("admin.categories.index")->with('error', 'Vui lòng chọn danh mục cần thao tác');
         }
+        //Kiểm tra xem ng dùng chọn hành động hay k
+        $act = $request->act;
+        if (!$act) {
+            return redirect()->route("admin.categories.index")->with('error', 'Vui lòng chọn hành động để thao tác');
+        }
+        $message = match ($act) {
+            'trash' => function () use ($listCheck) {
+                    Category::whereIn("id", $listCheck)->update(["is_active" => 0]);
+                    Category::destroy($listCheck);
+                    return 'Xoá thành công toàn bộ bản ghi đã chọn';
+                },
+            'active' => function () use ($listCheck) {
+                    Category::whereIn("id", $listCheck)->update(["is_active" => 1]);
+                    return 'Đăng toàn bộ những bản ghi đã chọn';
+                },
+            'inactive' => function () use ($listCheck) {
+                    Category::whereIn("id", $listCheck)->update(["is_active" => 0]);
+                    return 'Chuyển đổi toàn bộ những bản ghi về chờ xác nhận';
+                },
+            'restore' => function () use ($listCheck) {
+                    Category::onlyTrashed()->whereIn("id", $listCheck)->restore();
+                    return 'Khôi phục thành công toàn bộ bản ghi';
+                },
+            'forceDelete' => function () use ($listCheck) {
+                    Category::onlyTrashed()->whereIn("id", $listCheck)->forceDelete();
+                    return 'Xoá vĩnh viễn toàn bộ bản ghi khỏi hệ thống';
+                },
+            default => fn() => 'Hành động không hợp lệ',
+        };
+        return redirect()->route("admin.categories.index")->with('success', $message());
+    }
 
+
+    public function restore(string $id)
+    {
+        $category = Category::onlyTrashed()->find($id);
+        if (!$category) {
+            return redirect()->route('admin.categories.index')->with(['error' => 'category không tồn tại!']);
+        }
+        $category->restore();
+        return redirect()->route('admin.categories.index')->with(['success' => 'Khôi phục thành công!']);
+    }
+
+    public function forceDelete(string $id)
+    {
+        $category = Category::onlyTrashed()->find($id);
+        //Nếu Category đó không tồn tại thì báo lỗi
+        if (!$category) {
+            return redirect()->route('admin.categories.index')->with(['error' => 'category không tồn tại!']);
+        }
         if ($category->image) {
             $fileExists = Storage::disk('public')->exists($category->image);
             if ($fileExists) {
                 Storage::disk('public')->delete($category->image);
             }
         }
-
-        $category->delete();
-
-        return back()->with(['message' => 'Xóa thành công!']);
+        //Xoá vĩng viễn bản ghi ra khỏi hệ thống
+        $category->forceDelete();
+        return redirect()->route('admin.categories.index')->with(['success' => 'Xoá thành công']);
     }
 }
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -41,143 +41,167 @@
     </div>
     <!-- end page title -->
 
-    @if (session('message'))
-        <div class="alert alert-success" role="alert">
-            {{ session('message') }}
-        </div>
-    @endif
-
-    @if (session('error'))
-        <div class="alert alert-error" role="alert">
-            {{ session('error') }}
-        </div>
-    @endif
-
     <div class="row">
         <div class="col-lg-12">
-            <div class="row">
-                <div class="col-md-12 mb-3 mx-auto">
-                    <div class="float-end">
-                        <a href="{{ route('admin.categories.create') }}" class="btn btn-primary">Thêm mới</a>
-                    </div>
-                </div>
-            </div>
             <div class="card">
                 <div class="card-header d-flex justify-content-between gap-3">
-                        <div class="col-sm-auto d-flex">
-                            <select name="act" id="" class="form-select">
-                                <option value="" class="form-control">Thao tác nhiều bản ghi</option>
-                                {{-- @foreach ($listAct as $key => $act)
-                                    <option value="{{ $key }}" class="form-control">{{ $act }}</option>
-                                @endforeach --}}
-                            </select>
-                            <button type="submit" class="ms-2 btn btn-primary">Chose</button>
-                        </div>
-                        <div class="col-sm-auto d-flex ms-2">
-                            <ul class="d-flex gap-4 mt-1 list-unstyled">
-                                <li><a href="{{ request()->fullUrlWithQuery(['status' => 'all']) }}">Tất
-                                        cả()</a></li>
-                                <li><a href="{{ request()->fullUrlWithQuery(['status' => 'active']) }}">Kích
-                                        hoạt()</a></li>
-                                <li><a href="{{ request()->fullUrlWithQuery(['status' => 'inactive']) }}">Chờ xác
-                                        nhận()</a></li>
-                                <li><a href="{{ request()->fullUrlWithQuery(['status' => 'trash']) }}">Vô hiệu
-                                        hoá()</a></li>
-                            </ul>
-                        </div>
+                    <div class="col-sm-auto d-flex">
+                        <a href="{{ route('admin.categories.create') }}" class="btn btn-primary">Thêm mới</a>
                     </div>
+                    <div class="col-sm-auto d-flex ms-2">
+                        <ul class="d-flex gap-4 mt-1 list-unstyled">
+                            <li><a href="{{ request()->fullUrlWithQuery(['status' => 'all']) }}">Tất
+                                    cả({{ $count['all'] }})</a></li>
+                            <li><a href="{{ request()->fullUrlWithQuery(['status' => 'active']) }}">Kích
+                                    hoạt({{ $count['active'] }})</a></li>
+                            <li><a href="{{ request()->fullUrlWithQuery(['status' => 'inactive']) }}">Chờ xác
+                                    nhận({{ $count['inactive'] }})</a></li>
+                            <li><a href="{{ request()->fullUrlWithQuery(['status' => 'trash']) }}">Vô hiệu
+                                    hoá({{ $count['trash'] }})</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-auto d-flex ms-2">
+                        <form action="{{ route('admin.categories.index') }}" method="GET" class="d-flex gap-2">
+                            <input type="text" class="form-control ml-2" placeholder="Tìm kiếm ..." name="keyword"
+                                value="{{ request()->input('keyword') }}">
+                            <input type="hidden" name="status" value="{{ request()->input('status') }}">
+                            <button class="btn btn-outline-primary ms-2" type="submit">
+                                <i class="ri-search-line"></i>
+                            </button>
+                        </form>
+                    </div>
+                </div>
                 <div class="card-body">
-                    <table id="example" class="table table-bordered dt-responsive nowrap table-striped align-middle"
-                        style="width:100%">
-                        <thead>
-                            <tr>
-                                <th scope="col" style="width: 10px;">
-                                    <div class="form-check">
-                                        <input class="form-check-input fs-15" type="checkbox" id="checkAll" value="option">
-                                    </div>
-                                </th>
-                                <th data-ordering="false">Tên</th>
-                                <th data-ordering="false">Đường dẫn thân thiện</th>
-                                <th data-ordering="false">Ảnh</th>
-                                <th>Mô tả</th>
-                                <th>Danh mục cha</th>
-                                <th>Trạng thái</th>
-                                <th>Thao tác</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($categories as $category)
+                    <form action="{{ route('admin.categories.action') }}">
+                        @csrf
+                        <div class="row mb-3">
+                            <div class="col-md-4 d-flex">
+                                <select name="act" id="" class="form-select">
+                                    <option value="" class="form-control">Thao tác nhiều bản ghi</option>
+                                    @foreach ($listAct as $key => $act)
+                                        <option value="{{ $key }}" class="form-control">{{ $act }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                                <button class="btn btn-outline-primary ms-2" type="submit">
+                                    Chọn
+                                </button>
+                            </div>
+                        </div>
+
+                        <table id="example" class="table table-bordered dt-responsive nowrap table-striped align-middle"
+                            style="width:100%">
+                            <thead>
                                 <tr>
-                                    <th scope="row">
+                                    <th scope="col" style="width: 50px;">
                                         <div class="form-check">
-                                            <input class="form-check-input fs-15" type="checkbox" name="checkAll"
-                                                value="{{ $category->id }}">
+                                            <input class="form-check-input" type="checkbox" id="selectAll" value="option">
                                         </div>
                                     </th>
-                                    <td>{{ $category->name }}</td>
-                                    <td>
-                                        {{ $category->slug }}
-                                    </td>
-                                    <td>
-                                        <img src="{{ Storage::url($category->image) }}" width="100px" alt="">
-                                    </td>
-                                    <td>
-                                        {{ $category->description }}
-                                    </td>
-                                    <td>{{ $category->parent_id != null ? $category->parent->name : '' }}</td>
-                                    <td class="td_is_active">
-                                        @if ($category->is_active == 1)
-                                            <span class="badge rounded-pill bg-success">On</span>
-                                        @else
-                                            <span class="badge rounded-pill bg-danger">Off</span>
-                                        @endif
-                                    </td>
-
-                                    <td>
-                                        <div class="dropdown d-inline-block">
-                                            <button class="btn btn-soft-secondary btn-sm dropdown" type="button"
-                                                data-bs-toggle="dropdown" aria-expanded="false">
-                                                <i class="ri-more-fill align-middle"></i>
-                                            </button>
-                                            <ul class="dropdown-menu dropdown-menu-end">
-                                                <li><a href="{{ route('admin.categories.edit', ['category' => $category->id]) }}"
-                                                        class="dropdown-item edit-item-btn"><i
-                                                            class="ri-pencil-fill align-bottom me-2 text-muted"></i>
-                                                        Sửa</a></li>
-                                                <li>
-                                                    <form
-                                                        action="{{ route('admin.categories.destroy', ['category' => $category->id]) }}"
-                                                        method="post">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button class="dropdown-item remove-item-btn"
-                                                            onclick="return confirm('Xác nhận xóa ?')"><i
-                                                                class="ri-delete-bin-fill align-bottom me-2 text-muted"></i>
-                                                            Xóa</button>
-                                                    </form>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </td>
+                                    <th data-ordering="false">Tên</th>
+                                    <th data-ordering="false">Đường dẫn thân thiện</th>
+                                    <th data-ordering="false">Ảnh</th>
+                                    <th>Mô tả</th>
+                                    <th>Danh mục cha</th>
+                                    <th>Trạng thái</th>
+                                    <th>Thao tác</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody class="list form-check-all">
+                                @foreach ($categories as $category)
+                                    <tr>
+                                        <th scope="row">
+                                            <div class="form-check">
+                                                <input class="form-check-input checkbox" type="checkbox" name="listCheck[]"
+                                                    value="{{ $category->id }}">
+                                            </div>
+                                        </th>
+                                        <td>{{ $category->name }}</td>
+                                        <td>
+                                            {{ $category->slug }}
+                                        </td>
+                                        <td>
+                                            <img src="{{ Storage::url($category->image) }}" width="100px" alt="">
+                                        </td>
+                                        <td>
+                                            {{ $category->description }}
+                                        </td>
+                                        <td>
+                                            <span class="badge bg-primary">
+                                                {{ $category->parent_id != null ? "Danh mục con": 'Danh mục cha' }}
+                                            </span>
+                                        </td>
+                                        <td class="td_is_active">
+                                            @if ($category->is_active == 1)
+                                                <span class="badge rounded-pill bg-success">On</span>
+                                            @else
+                                                <span class="badge rounded-pill bg-danger">Off</span>
+                                            @endif
+                                        </td>
+
+                                        <td>
+                                            <div class="dropdown d-inline-block">
+                                                <button class="btn btn-soft-secondary btn-sm dropdown" type="button"
+                                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                                    <i class="ri-more-fill align-middle"></i>
+                                                </button>
+                                                <ul class="dropdown-menu dropdown-menu-end">
+                                                    @if (request()->status == 'trash')
+                                                        <li><a href="{{ route('admin.categories.restore', ['id' => $category->id]) }}"
+                                                                onclick="return confirm('Bạn có muốn khôi phục bản ghi {{ $category->title }} không ?')"
+                                                                class="dropdown-item edit-item-btn"><i
+                                                                    class="ri-restart-fill align-bottom me-2 text-muted"></i>
+                                                                Khôi phục</a></li>
+                                                        <li><a href="{{ route('admin.categories.forceDelete', ['id' => $category->id]) }}"
+                                                                onclick="return confirm('Bạn có muốn xoá vĩnh viễn bản ghi {{ $category->title }} không ?')"
+                                                                class="dropdown-item edit-item-btn"><i
+                                                                    class="ri-delete-bin-line align-bottom me-2 text-muted"></i>
+                                                                Xoá vĩnh viễn</a></li>
+                                                    @else
+                                                        <li><a href="{{ route('admin.categories.edit', ['category' => $category->id]) }}"
+                                                                class="dropdown-item edit-item-btn"><i
+                                                                    class="ri-pencil-fill align-bottom me-2 text-muted"></i>
+                                                                Sửa</a></li>
+                                                        <li>
+                                                        <li><a href="{{ route('admin.categories.destroy', ['category' => $category->id]) }}"
+                                                                class="dropdown-item remove-item-btn"><i
+                                                                    class="ri-delete-bin-fill align-bottom me-2 text-muted"></i>
+                                                                Xoá</a></li>
+                                                    @endif
+
+                                                </ul>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </form>
+
                     <div class="paginate-data">
                         {{ $categories->links() }}
                     </div>
                 </div>
+
             </div>
         </div><!--end col-->
     </div><!--end row-->
 @endsection
 
 @section('script-libs')
+    <script>
+        document.getElementById('selectAll').addEventListener('change', function() {
+            var checkboxes = document.querySelectorAll('.checkbox');
+            for (var checkbox of checkboxes) {
+                checkbox.checked = this.checked;
+            }
+        });
+    </script>
     <!--datatable js-->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
-    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    {{-- <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script> --}}
     <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
