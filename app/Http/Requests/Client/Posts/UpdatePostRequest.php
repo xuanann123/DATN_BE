ea31a721c81f2bf8e75e5da3254c050979aaae86
xuananfpt <annxph37114@fpt.edu.vn>
2024-10-08 14:18:26 +0700

    refactor/update_api_posts

---
+++ app/Http/Requests/Client/Posts/UpdatePostRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Client\Posts;
+
+use App\Http\Requests\BaseFormRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePostRequest extends BaseFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string|min:6',
+            'slug' => 'required|string|max:255|unique:posts,slug,' . $this->post->id,
+            'thumbnail' => 'nullable|image',
+            'status' => 'nullable|string',
+            'published_at' => 'nullable|date',
+            'is_active' => 'nullable|boolean',
+            'allow_comments' => 'nullable|boolean',
+            'content' => 'required|string',
+            'categories' => 'required|array',
+            'categories.*' => 'exists:categories,id',
+            'tags' => 'nullable|array',
+            'tags.*' => 'nullable',
+        ];
+    }
+}
