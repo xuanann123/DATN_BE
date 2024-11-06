49ba575c53f0b463b304a3e8137aafd6586425ce
vuxuanduc <vuxuanduc54@gmail.com>
2024-11-06 15:31:18 +0700

    fix_api_payment

--- app/Http/Requests/Client/Posts/CommentRequest.php
+++ app/Http/Requests/Client/Posts/CommentRequest.php
@@ -23,19 +23,21 @@ class CommentRequest extends FormRequest
     {
         return [
             'id_user' => 'required|integer|exists:users,id',
-            'content' => 'required|string|max:255',
+            'content' => 'required|string|min:2|max:255',
             'commentable_id' => 'required|integer|exists:posts,id',
             'parent_id' => 'nullable|integer|exists:comments,id',
         ];
     }
 
-    public function messages(): array {
+    public function messages(): array
+    {
         return [
             'id_user.required' => 'Vui lòng thêm id người dùng.',
             'id_user.integer' => 'Id người dùng là số nguyên.',
             'id_user.exists' => 'Người dùng không tồn tại',
             'content.required' => 'Vui lòng nhập bình luận',
             'content.string' => 'Bình luận phải là chuỗi.',
+            'content.min' => 'Bình luận phải trên 2 kí tự.',
             'content.max' => 'Bình luận không quá 255 kí tự.',
             'commentable_id.required' => 'Vui lòng thêm id bài viết.',
             'commentable_id.integer' => 'Id bài viết là số nguyên.',
