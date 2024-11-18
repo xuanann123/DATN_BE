a88411a5eff82bd3e85ab9e2c84dce5eb3eac80c
xuananfpt <annxph37114@fpt.edu.vn>
2024-11-19 00:35:04 +0700

    feature/config_api_my_course_bought

--- routes/api.php
+++ routes/api.php
@@ -184,8 +184,6 @@ Route::middleware('auth:sanctum')->group(function () {
         //Đăng kí thành giảng viên
         Route::post('/register-teacher', [UserController::class, 'registerTeacher']);
         
-
-
         //Check lịch sử đang học đến bài nàp
         Route::get('/check-history-learning', [UserController::class, 'checkLearning']);
 
