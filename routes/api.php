7b5d224a3a51f5724e375948f4bdd8e320c5b3ef
tus204-dev <gpttuonthi123@gmail.com>
2024-11-24 11:22:49 +0700

    feat/api-client-download-resourse-for-doc-lesson

--- routes/api.php
+++ routes/api.php
@@ -81,7 +81,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     # ===================== ROUTE FOR LEARNING PATH ===========================
     Route::prefix('learning-path')->group(function () {
-        
+
         Route::get('/list-category', [CourseController::class, 'getListCategory']);
         Route::get('/list-course-by-learning-path/{slug}', [CourseController::class, 'getListCourseByLearningPath']);
 
@@ -129,6 +129,7 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('quiz/check-quiz', [LessonController::class, 'checkQuiz']);
         Route::put('quiz/quiz-progress/{quiz}', [LessonController::class, 'updateQuizProgress']);
         Route::get('quiz/result/{userId}/{quizId}', [LessonController::class, 'getQuizResult']);
+        Route::get('{lesson}/download-resourse', [LessonController::class, 'downloadResource']);
     });
 
     # ===================== ROUTE FOR NOTE ===========================
